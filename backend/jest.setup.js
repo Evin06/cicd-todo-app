2c0435ba33d0d4d89c8113c60f2e013be0f2c334
philippe-hjik <122774959+philippe-hjik@users.noreply.github.com>
2024-11-27 15:02:28 +0100

    test worflow

--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();  // Charge le fichier .env
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-// Connexion à une base de données temporaire (utilisée uniquement pour les tests)
+let mongo = undefined;
+
 beforeAll(async () => {
-    const url = process.env.DB_URL; // Base de données pour les tests
-    await mongoose.connect(url);
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+  await mongoose.connect(uri);
 });
 
 // Déconnexion de la base de données après tous les tests
